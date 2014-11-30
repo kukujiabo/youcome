f1e0175da522619e9bf4044605fd37e540508f7c
meroc <kukujiabo@163.com>
2014-11-30 12:11:13 +0800

    rm all mongo-script

--- mongo-scripts/insert_ali.js
+++
@@ -1,4 +0,0 @@
-db.ali.insert({
-  url: 'http://shop1414037216529.1688.com/',
-  active: 1
-});
--- mongo-scripts/insert_brand.js
+++
@@ -1,6 +0,0 @@
-db.catagories.insert({
-  name: '红酒',
-  desc: '',
-  active: 1,
-  images: 'wine.jpg'
-});
